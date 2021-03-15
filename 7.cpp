2422891f422c82e801d77f02c73cb619014f93c1
golnoushnb <80672398+golnoushnb@users.noreply.github.com>
2021-03-15 12:55:00 +0330

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,10 +1,18 @@
 #include<stdio.h>
+#include <assert.h>
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5 };
 	int *p = arr;
+	assert(*p == 1);
 	++*p;
+	assert(*p == 2);
 	p += 2;
+	assert(*p == 3);
 	printf("%d", *p);
 	return 0;
 }
+/* 
+    No ERRORS...
+    output : 3
+*/
