0b9a9cf707efdd16d1c074695e76ee8ae61e9420
Narges81 <narges.montazeri81@gmail.com>
2021-03-15 19:50:04 +0330

    debug files

--- 3.cpp
+++ 3.cpp
@@ -1,22 +1,24 @@
 ﻿#include<stdio.h>
 #include<stdlib.h>
+#include<assert.h>
 #define MAX_SIZE 200
 int arr[MAX_SIZE];
 
-typedef struct alfa * alfaptr;
+typedef struct alfa* alfaptr;
 
 struct alfa {
 	long long x;
 	alfaptr next;
 };
 alfaptr rear = NULL, front = NULL;
-void push(int x)
+void push(long long int x)
 {
 	alfaptr node;
 	node = (alfaptr)malloc(sizeof(struct alfa));
 	node->x = x;
+	node->next = NULL;
 	if (!front)
-		front = node;
+		front = rear = node;
 	else {
 		rear->next = node;
 		rear = node;
@@ -26,34 +28,38 @@ void push(int x)
 void pop()
 {
 	alfaptr node;
-	if (!front)
-		printf("ERROR1");
-	else
-	{
-		node = front->next;
-		front = node;
-	}
+	assert(front);
+	node = front->next;
+	free(front);
+	front = node;
 }
-void search(int x)
+
+void search(long long int x)
 {
 	alfaptr node = front;
 	int counter = 0;
+	assert(node);
 	while (node)
+	{
 		if (node->x == x)
-			printf("%d", counter);
-		else {
-			printf("ERROR2");
+		{
+			printf("%d\n", counter);
 			break;
 		}
+		assert(node);
 		node = node->next;
+		counter++;
+	}
 }
 
 void rpop() {//pop last element
 	alfaptr node = front;
-	while (node)
+	assert(front);
+	while (node->next && node->next != rear)
 		node = node->next;
 	free(rear);
 	rear = node;
+	node->next = NULL;
 }
 
 void set()
@@ -66,29 +72,30 @@ void set()
 int size()
 {
 	alfaptr node = front;
-	int count;
+	int count = 0;
 	while (node)
-		count++;node = node->next;
+	{
+		count++;
+		node = node->next;
+	}
 	return count;
 }
 
 void show()
 {
-	if (!front) {
-		for (int i = 0; i < MAX_SIZE; i++)
-			printf("%d ", arr[i]);
-	}
-	else
-	{
-		printf("ERROR3");
-	}
+	assert(front);
+	alfaptr node = front;
+	for (int i = 0; i < MAX_SIZE && node; i++, node = node->next)
+		printf("%d ", arr[i]);
+	printf("\n");
 }
 
-int average()
+float average()
 {
 
 	alfaptr node = front;
-	int sum = 0, count;
+	int sum = 0, count = 0;
+	assert(front);
 	while (node) {
 		sum += node->x;
 		count++;
@@ -103,6 +110,7 @@ void main()
 	long long int x;
 	while (true)
 	{
+		printf("command:");
 		scanf("%d", &cmd);
 		switch (cmd)
 		{
@@ -127,9 +135,12 @@ void main()
 			show();
 			break;
 		case 7://size
-			printf("%d", size());
+			printf("%d\n", size());
 			break;
-		case 10:
+		case 8:
+			printf("%f\n", average());
+			break;
+		default:
 			exit(0);
 		}
 	}
