fb661142710fc3b4285074ebd08cea5bf26b6748
mahsaheli <80674520+mahsaheli@users.noreply.github.com>
2021-03-16 18:00:49 +0330

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 // count all the specific char in the whole array of strings
 int countAllSpecificChars(string sArr[], int arrLength, char specificChar) {
-    int count;
-    for (int i = 0; i <= arrLength; ++i)
-        for (int j = 0; j <= sArr[i].size(); ++j)
+    int count=0;//bedoone meghdar dehi avalie ndmitavan meghdar motaghayer ra afzayesh dad(count=0)
+    for (int i = 0; i < arrLength; ++i)// i < arrLength mosavi nabayad dashte bashad
+        for (int j = 0; j < sArr[i].size(); ++j)// j < sArr[i].size() mosavi nabayad dashte bashad
             // if the jth char of the string is the specific char
-            if (sArr[i][j] = specificChar)
+            if (sArr[i][j] == specificChar)//baraye barresi shart daroon if bayad az == estefade shavad 
                 count++;
     return count;
 }
