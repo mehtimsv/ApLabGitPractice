93f1be527fb67d4c508fcac4945e7f51bfb6bd61
MrVein <75935224+MrVein@users.noreply.github.com>
2021-03-16 16:13:23 +0330

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 // count all the specific char in the whole array of strings
 int countAllSpecificChars(string sArr[], int arrLength, char specificChar) {
-    int count;
-    for (int i = 0; i <= arrLength; ++i)
+    //count should be initialized to 0
+    int count=0;
+    // j should not be equal to arrLength because it starts from 0.
+    for (int i = 0; i < arrLength; ++i)
         for (int j = 0; j <= sArr[i].size(); ++j)
             // if the jth char of the string is the specific char
-            if (sArr[i][j] = specificChar)
+            // same old bug : '=' to '=='
+            if (sArr[i][j] == specificChar)
                 count++;
     return count;
 }
