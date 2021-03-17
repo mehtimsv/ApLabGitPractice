1315c3d271951b37bfbaaec164e0d16f048509e4
pardis-yri <80654355+pardis-yri@users.noreply.github.com>
2021-03-17 03:33:02 -0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 #include <string>
+#pragma warning (disable:4996)
 
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+FILE* pFile = fopen("mylogg.txt", "a");
+
+#define debug_printf(fmt, ...) \
+do { if (DEBUG) { \
+fprintf(pFile, fmt, __VA_ARGS__); \
+fclose(pFile); } } while (0)
 using namespace std;
 
 // count all the specific char in the whole array of strings
 int countAllSpecificChars(string sArr[], int arrLength, char specificChar) {
-    int count;
-    for (int i = 0; i <= arrLength; ++i)
-        for (int j = 0; j <= sArr[i].size(); ++j)
-            // if the jth char of the string is the specific char
-            if (sArr[i][j] = specificChar)
-                count++;
-    return count;
+	int count = 0;/// Count meghdar dehi Avalie nashode.
+	for (int i = 0; i < arrLength; ++i)///= ro bardashtam.
+		for (int j = 0; j < sArr[i].length(); ++j)// = ro bardashtam .
+			// if the jth char of the string is the specific char
+			if (sArr[i][j] == specificChar)/// = ezafe kardam dakhele if
+				count++;
+	return count;
 }
 
 int main() {
-    string sArr[4] = {
-            "I am",
-            "in",
-            "ap",
-            "class"
-    };
-    char findIt;
-    cin >> findIt;
-    cout << countAllSpecificChars(sArr, 4, findIt);
+	string sArr[4] = {
+			"I am",
+			"in",
+			"ap",
+			"class"
+	};
+debug_printf("%d\n", sArr[0].size());///baraye test.
+	char findIt;
+	cin >> findIt;
+  
+	cout << countAllSpecificChars(sArr, 4, findIt);
 }
