69b47659f5afb0e16f8d236851f9e3d773b1e3a0
nosi <manosrati1380@gmail.com>
2021-03-17 21:47:18 +0330

    code debug

--- 1.cpp
+++ 1.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-long long *b;
-
 long long int factorial(int n)
 {
 	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
@@ -15,7 +13,7 @@ long long int *producingTheFactorialFractions()
 {
     long long b[10];
 
-	for (int i = 10; i >= 0; i--)
+	for (int i = 9; i >= 0; i--)
 	{
 		b[i] += (int)pow(factorial(10), 2.0) / (i + 1);
 	}
@@ -35,10 +33,11 @@ int main()
 {
 
 	long long int *a;
-	a = producingTheFactorialFractions();
+	
 	checkZeros(a);
 	for (int i = 0; i < 10; i++)
 	{
+		a = producingTheFactorialFractions();
 		cout << a[i] << endl;
 	}
 	delete a;
