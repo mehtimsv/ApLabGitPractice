a90d2d139ea563dcab6902deb40495e829c3a460
rezvanabedin <rezvanabedin79@gmail.com>
2021-03-17 17:52:09 -0700

    debugging2

--- 8.cpp
+++ 8.cpp
@@ -1,10 +1,12 @@
 ﻿#include<stdio.h>
+#include<assert.h>
 const char * f(const char **p) {
 	auto q = (p + sizeof(char))[1];
 	return q;
 }
 int main() {
 	const char * str[] = { "Wish","You","Best",":D" };
+	assert(*f(str) == 'B');
 	printf("%c%c ", *f(str), *(f(str) + 1));
 	printf("%c%c%c%c\n", **str, *(*(str + 1) + 1), *((str + 2)[-1] + 1), **&*(&str[-1] + 1));
 }
