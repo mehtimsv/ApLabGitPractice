38fdda223b22699f28cd5a2d908b75d1a70db671
MacBook <macbook@MacBooks-MacBook.local>
2021-03-18 13:19:50 +0330

    Debug

--- 1.cpp
+++ 1.cpp
@@ -3,48 +3,27 @@
 #include <math.h>
 
 using namespace std;
-
 long long *b;
+long long int factorial(int n) {
+    return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;}
 
-long long int factorial(int n)
-{
-	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-}
+long long int *producingTheFactorialFractions() {
+    static long long b[10];
+    for (int i = 10; i >= 0; i--) {
+        b[i] = (long long)pow(factorial(10), 2.0) / (i+1); }
+    return b; }
+void checkZeros(long long *a) {
+    for (int i = 9; i >= 0; i--) {
+        if (a[i] == 0)
+            cout << "Zero Found" << endl;
+    } }
+int main() {
+    long long int *a;
+    a = producingTheFactorialFractions();
+    checkZeros(a);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << endl; }
+    a=NULL;
+    delete a; }
 
-long long int *producingTheFactorialFractions()
-{
-    long long b[10];
-
-	for (int i = 10; i >= 0; i--)
-	{
-		b[i] += (int)pow(factorial(10), 2.0) / (i + 1);
-	}
-	return b;
-}
-
-void checkZeros(long long *a)
-{
-	for (int i = 9; i >= 0; i--)
-	{
-		if (a[i] = 0)
-			cout << "Zero Found" << endl;
-	}
-}
-
-int main()
-{
-
-	long long int *a;
-	a = producingTheFactorialFractions();
-	checkZeros(a);
-	for (int i = 0; i < 10; i++)
-	{
-		cout << a[i] << endl;
-	}
-	delete a;
-
-	cout<<"hello";
-	cout<<"Bye";
-
-
-}
