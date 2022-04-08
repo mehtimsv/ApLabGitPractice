1bb4d2fde34506d8acbb40e73465bb253be29204
mhtcode <masihtanoursaz@gmail.com>
2022-04-08 15:50:36 +0430

    A number of bugs were fixed, but the program crashes at the end

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <string>
 #include <math.h>
-
+#include <cstdlib>
 using namespace std;
-
-long long *b;
 
 long long int factorial(int n)
 {
 	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
-long long int *producingTheFactorialFractions()
+long long int *producingTheFactorialFractions(long long int *b)
 {
-    long long b[10];
-
-	for (int i = 10; i >= 0; i--)
+	for (int i = 9; i >= 0; i--) // changed 10 to 9
 	{
-		b[i] += (int)pow(factorial(10), 2.0) / (i + 1);
+		b[i] += (long long int)pow((double)factorial(10), 2.0) / (i + 1); // change int to long long int
 	}
 	return b;
 }
@@ -26,25 +21,22 @@ void checkZeros(long long *a)
 {
 	for (int i = 9; i >= 0; i--)
 	{
-		if (a[i] = 0)
+		if (a[i] == 0) // change = to ==
 			cout << "Zero Found" << endl;
 	}
 }
 
 int main()
 {
-
-	long long int *a;
-	a = producingTheFactorialFractions();
+	long long int *a = new long long int; // use heap
+	a = producingTheFactorialFractions(a);
 	checkZeros(a);
 	for (int i = 0; i < 10; i++)
 	{
 		cout << a[i] << endl;
 	}
-	delete a;
-
-	cout<<"hello";
-	cout<<"Bye";
-
-
+	cout << "hello" << endl;
+	cout << "Bye";
+	delete a; // for leak of memory
+	return 0;
 }
