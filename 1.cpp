6fecf67e34d6731693af301f36262f4289f6d025
unknown <arefepourmohammdi@gmail.com>
2022-04-10 20:52:18 +0430

    i made some changes in this file

--- 1.cpp
+++ 1.cpp
@@ -4,29 +4,31 @@
 
 using namespace std;
 
-long long *b;
+long long int* b;// why did you define it ?:) anyway let this variable live it will change nothing
 
-long long int factorial(int n)
+long long int factorial(int n)//i think this function is ok
 {
 	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
 long long int *producingTheFactorialFractions()
 {
-    long long b[10];
+    long long int *b = new long long int  [10];// i changed it to a pointer . so no error accured
 
-	for (int i = 10; i >= 0; i--)
+	for (int i = 9; i >= 0; i--) // i starts from 9 not 10
 	{
-		b[i] += (int)pow(factorial(10), 2.0) / (i + 1);
+		b[i] = (long long int)pow(factorial(10), 2) / (i + 1);// i changed int to a long long int
+
 	}
+
 	return b;
 }
 
-void checkZeros(long long *a)
+void checkZeros(long long int* a)
 {
 	for (int i = 9; i >= 0; i--)
 	{
-		if (a[i] = 0)
+		if (a[i] == 0)// here must be == not =
 			cout << "Zero Found" << endl;
 	}
 }
@@ -34,7 +36,7 @@ void checkZeros(long long *a)
 int main()
 {
 
-	long long int *a;
+	long long int *a = new long long int[10]; // must use new
 	a = producingTheFactorialFractions();
 	checkZeros(a);
 	for (int i = 0; i < 10; i++)
