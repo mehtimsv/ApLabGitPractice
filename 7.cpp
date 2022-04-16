e9fe37d37363f893964df93402c6b01f0a8b58a4
AlirezaPeymanirad <103421138+AlirezaPeymanirad@users.noreply.github.com>
2022-04-16 13:32:07 +0430

    edit

--- 7.cpp
+++ 7.cpp
@@ -2,9 +2,17 @@
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5 };
-	int *p = arr;
-	++*p;
+	int* p = arr;
+	++* p;
 	p += 2;
 	printf("%d", *p);
 	return 0;
 }
+//////////////////////////////////  OUTPUT  ///////////////////////////////////////////////////////////////
+//
+//   3
+//
+//   E:\Users\Alireza\source\repos\Project2\Debug\Project2.exe (process 1600) exited with code 0.
+//   Press any key to close this window . . .
+//                                                                                                  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
--- 8.cpp
+++ 8.cpp
@@ -1,13 +1,21 @@
 ﻿#include<stdio.h>
-const char * f(const char **p) {
+const char* f(const char** p) {
 	auto q = (p + sizeof(char))[1];
 	return q;
 }
 int main() {
-	const char * str[] = { "Wish","You","Best",":D" };
+	const char* str[] = { "Wish","You","Best",":D" };
 	printf("%c%c ", *f(str), *(f(str) + 1));
 	printf("%c%c%c%c\n", **str, *(*(str + 1) + 1), *((str + 2)[-1] + 1), **&*(&str[-1] + 1));
 
 
-	
+
 }
+//////////////////////////////////  OUTPUT  ///////////////////////////////////////////////////////////////
+//
+//   Be WooW
+//
+//   E:\Users\Alireza\source\repos\Project2\Debug\Project2.exe(process 8276) exited with code 0.
+//   Press any key to close this window . . .
+//                                                                                                  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
